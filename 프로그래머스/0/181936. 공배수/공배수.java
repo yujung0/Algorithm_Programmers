cc19f73d32b5c4f05a761efc697f0a7c23979bcc
Jang Yu Jung <137154455+yujung0@users.noreply.github.com>
2024-01-24 17:10:12 +0900

    [level 0] Title: 공배수, Time: 0.03 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        
+        if(number%n == 0 && number%m == 0) {
+            return 1;
+        } else{
+            return 0;
+
+        }
+        
+        // int answer = 0;
+        // return answer;
+    }
+}
