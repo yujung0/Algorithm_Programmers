628bb54fa0d4f684dbe9b4d6293fc53f6d494c1f
Jang Yu Jung <137154455+yujung0@users.noreply.github.com>
2024-01-24 17:02:35 +0900

    [level 0] Title: n의 배수, Time: 0.01 ms, Memory: 64.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num, int n) {
+     
+       if(num % n == 0) {
+            return 1;
+        } else{
+            return 0;
+
+        }
+        // int answer = 0;
+        // return answer;
+    }
+}
